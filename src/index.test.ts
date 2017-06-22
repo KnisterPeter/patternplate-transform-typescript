44c87690fb1d3d81b1aef70eb2301cf1a792e4b7
Malte Warncke <malte.warncke@sinnerschrader.com>
2017-06-22 11:18:08 +0200

    fix: folder deepth for declarations fixed

--- src/index.test.ts
+++ src/index.test.ts
@@ -129,3 +129,108 @@ test('writeDeclaration should rewrite dependency paths on a best guess', t => {
 
   t.deepEqual(application.resources[0].content, expected);
 });
+
+test('writeDeclaration should rewrite multiple dependency paths on a best guess', t => {
+  const expected = stripIndents`
+    import Type1 from '../dependency-folder/index';
+    import { Type1a } from '../dependency-folder/index';
+    import * as Type1b from '../dependency-folder/index';
+    import Type2 = require('../dependency-folder/index');
+    const Type3 = require('../dependency-folder/index');
+    import Type4 from '../second-folder/index';
+    import { Type4a } from '../second-folder/index';
+    import * as Type4b from '../second-folder/index';
+    import Type5 = require('../second-folder/index');
+    const Type6 = require('../second-folder/index');
+    import Type7 from '../another-folder/third-folder/index';
+    import { Type7a } from '../another-folder/third-folder/index';
+    import * as Type7b from '../another-folder/third-folder/index';
+    import Type8 = require('../another-folder/third-folder/index');
+    const Type9 = require('../another-folder/third-folder/index');
+  `;
+
+  const input = getFileMock(``);
+  input.path = '/tmp/patterns/input/index.tsx';
+  input.pattern.manifest.patterns = {
+    dependency: 'dependency-folder',
+    secondDependency: 'second-folder',
+    thirdDependency: 'another-folder/third-folder'
+  };
+  input.dependencies['dependency'] = getFileMock('', 'dependency-folder');
+  input.dependencies['dependency'].path = '/tmp/patterns/dependency-folder/index.tsx';
+  input.dependencies['secondDependency'] = getFileMock('', 'second-folder');
+  input.dependencies['secondDependency'].path = '/tmp/patterns/second-folder/index.tsx';
+  input.dependencies['thirdDependency'] = getFileMock('', 'third-folder');
+  input.dependencies['thirdDependency'].path = '/tmp/patterns/another-folder/third-folder/index.tsx';
+  const application = getApplicationMock();
+  application.resources = [];
+  const output = {
+    status: 0,
+    outputText: '',
+    declarationText: stripIndents`
+      import Type1 from 'dependency';
+      import { Type1a } from 'dependency';
+      import * as Type1b from 'dependency';
+      import Type2 = require('dependency');
+      const Type3 = require('dependency');
+      import Type4 from 'secondDependency';
+      import { Type4a } from 'secondDependency';
+      import * as Type4b from 'secondDependency';
+      import Type5 = require('secondDependency');
+      const Type6 = require('secondDependency');
+      import Type7 from 'thirdDependency';
+      import { Type7a } from 'thirdDependency';
+      import * as Type7b from 'thirdDependency';
+      import Type8 = require('thirdDependency');
+      const Type9 = require('thirdDependency');
+    `
+  };
+
+  (createTypescriptTransform as any).writeDeclaration(input, output, application);
+  console.log(application.resources[0].content);
+
+  t.deepEqual(application.resources[0].content, expected);
+});
+
+test('writeDeclaration should rewrite multiple and deeper dependency paths on a best guess', t => {
+  const expected = stripIndents`
+    import Type1 from '../dependency-folder/index';
+    import Type2 = require('../../package-two/second-folder/index');
+    const Type3 = require('../../package-two/another-folder/third-folder/index');
+    import Type4 from '../../package-three/another-folder/fourth-folder/index';
+  `;
+
+  const input = getFileMock(``);
+  input.path = '/tmp/patterns/package-one/input/index.tsx';
+  input.pattern.manifest.patterns = {
+    dependency: 'package-one/dependency-folder',
+    secondDependency: 'package-two/second-folder',
+    thirdDependency: 'package-two/another-folder/third-folder',
+    fourthDependency: 'package-three/another-folder/fourth-folder'
+  };
+  input.dependencies['dependency'] = getFileMock('', 'dependency-folder');
+  input.dependencies['dependency'].path = '/tmp/patterns/package-one/dependency-folder/index.tsx';
+  input.dependencies['secondDependency'] = getFileMock('', 'second-folder');
+  input.dependencies['secondDependency'].path = '/tmp/patterns/package-two/second-folder/index.tsx';
+  input.dependencies['thirdDependency'] = getFileMock('', 'third-folder');
+  input.dependencies['thirdDependency'].path = '/tmp/patterns/package-two/another-folder/third-folder/index.tsx';
+  input.dependencies['thirdDependency'] = getFileMock('', 'third-folder');
+  input.dependencies['thirdDependency'].path = '/tmp/patterns/package-three/another-folder/fourth-folder/index.tsx';
+  const application = getApplicationMock();
+  application.resources = [];
+  const output = {
+    status: 0,
+    outputText: '',
+    declarationText: stripIndents`
+      import Type1 from 'dependency';
+      import Type2 = require('secondDependency');
+      const Type3 = require('thirdDependency');
+      import Type4 from 'fourthDependency';
+    `
+  };
+
+  (createTypescriptTransform as any).writeDeclaration(input, output, application);
+  console.log(application.resources[0].content);
+
+  t.deepEqual(application.resources[0].content, expected);
+});
